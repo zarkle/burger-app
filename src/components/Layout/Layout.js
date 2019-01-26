e4c9883569aa54a59344fb8b5ae432b10bd76102
Beverly Pham <beverly.pham@gmail.com>
2019-01-25 19:38:23 -0800

    make hoc Aux component to wrap layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import Aux from '../../hoc/Aux';
+
 const layout = (props) => {
-  <div>Toolbar, SideDrawer, Backdrop</div>
-  <main>
-    {props.children}
-  </main>
+  <Aux>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main>
+        {props.children}
+    </main>
+  </Aux>
 };
 
 export default layout;
