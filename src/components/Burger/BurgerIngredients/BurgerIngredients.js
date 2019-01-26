2a337ed64aa8dc6b7ed317747779e31c798003b4
Beverly Pham <beverly.pham@gmail.com>
2019-01-25 20:31:49 -0800

    start burger ingredients component

---
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './BurgerIngredients.module.css'
+
+const burgerIngredient = (props) => {
+  let ingredients;
+  
+  switch (props.type) {
+    case ('bun-bottom'):
+      ingredients = <div className={styles.BunBottom}></div>;
+      break;
+    case ('bun-top'):
+      ingredients = (
+        <div className={styles.BunTop}>
+          <div className={styles.Seeds1}></div>
+          <div className={styles.Seeds2}></div>
+        </div>
+      );
+      break;
+    case ('meat'):
+      ingredients = <div className={styles.Meat}></div>;
+      break;
+    case ('cheese'):
+      ingredients = <div className={styles.Cheese}></div>;
+      break;
+    case ('salad'):
+      ingredients = <div className={styles.Salad}></div>;
+      break;
+    case ('bacon'):
+      ingredients = <div className={styles.Bacon}></div>;
+      break;
+    default:
+      ingredients = null;
+  };
+  return ingredients;
+};
+
+export default burgerIngredient;
