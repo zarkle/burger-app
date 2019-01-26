b44a27b6aa6c16ed624c0867ed3e6e60ba32583a
Beverly Pham <beverly.pham@gmail.com>
2019-01-25 22:54:13 -0800

    less button is disabled if that ingredient is not on burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -36,6 +36,7 @@ class BurgerBuilder extends Component {
   
   removeIngredientHandler = (type) => {
     const oldCount = this.state.ingredients[type];
+    if (oldCount <= 0) {return}
     const newCount = oldCount - 1;
     const updatedIngredients = {
       ...this.state.ingredients
@@ -48,12 +49,19 @@ class BurgerBuilder extends Component {
   };
   
   render() {
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;  // convert number to boolean
+    }
     return(
       <Aux>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls 
           ingredientAdded={this.addIngredientHandler} 
-          ingredientRemoved={this.removeIngredientHandler} />
+          ingredientRemoved={this.removeIngredientHandler} 
+          disabled={disabledInfo} />
       </Aux>
     );
   }
