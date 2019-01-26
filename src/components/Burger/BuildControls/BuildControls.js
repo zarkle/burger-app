924ec7217b0bc6fc727b9edaa527274333ceb94b
Beverly Pham <beverly.pham@gmail.com>
2019-01-25 22:24:39 -0800

    add build control components to burger builder

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import styles from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+  {label: 'Salad', type: 'salad'},
+  {label: 'Bacon', type: 'bacon'},
+  {label: 'Cheese', type: 'cheese'},
+  {label: 'Meat', type: 'meat'},
+];
 
 const buildControls = (props) => (
-  <div>
-    
+  <div className={styles.BuildControls}>
+    {controls.map(control => (
+      <BuildControl key={control.label} label={control.label} />
+    ))}
   </div>
 );
 
