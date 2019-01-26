849a664a58370273446936070e8051cc8a3e71a8
Beverly Pham <beverly.pham@gmail.com>
2019-01-25 20:07:56 -0800

    add a CSS module for Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
 import Aux from '../../hoc/Aux';
+import styles from './Layout.module.css';
 
 const layout = (props) => (
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
+    <main className={styles.Content}>
         {props.children}
     </main>
   </Aux>
