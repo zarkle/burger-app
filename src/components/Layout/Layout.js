95eeb2272e3d886e26bdab6f4f54de2653d364f0
Beverly Pham <beverly.pham@gmail.com>
2019-01-25 19:46:14 -0800

    add Aux component to Layout and Layout component to App.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import Aux from '../../hoc/Aux';
 
-const layout = (props) => {
+const layout = (props) => (
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
     <main>
         {props.children}
     </main>
   </Aux>
-};
+);
 
 export default layout;
