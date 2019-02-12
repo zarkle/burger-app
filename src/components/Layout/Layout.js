29c585efa834402d352ecfc3c8c420c00c248827
Beverly Pham <beverly.pham@gmail.com>
2019-02-12 15:13:40 -0800

    convert layout component to class instead of function

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import styles from './Layout.module.css';
 
-const layout = (props) => (
-  <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <main className={styles.Content}>
-      {props.children}
-    </main>
-  </Aux>
-);
+class Layout extends Component {
+  render() {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer />
+        <main className={styles.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    )
+  }
+}
 
-export default layout;
+export default Layout;
