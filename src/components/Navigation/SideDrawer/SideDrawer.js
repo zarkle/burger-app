d1dea14306dea5f21cf25f7c6822c15dfc31cb30
Beverly Pham <beverly.pham@gmail.com>
2019-02-12 15:10:34 -0800

    add backdrop to sidedrawer

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,18 +2,23 @@ import React from 'react';
 import styles from './SideDrawer.module.css';
 import Logo from '../../Logo/Logo';
 import NavItems from '../../Navigation/NavItems/NavItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props) => {
   
   return (
-    <div className={styles.SideDrawer}>
-      <div className={styles.Logo}>
-        <Logo />
+    <Aux>
+      <Backdrop show />
+      <div className={styles.SideDrawer}>
+        <div className={styles.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavItems />
+        </nav>
       </div>
-      <nav>
-        <NavItems />
-      </nav>
-    </div>
+    </Aux>
   );
 };
 
--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styles from './Backdrop.module.css';
 
-const backdrop = (props) => {
-  return (props.show) ? <div className={styles.Backdrop} onClick={props.clicked}></div>  : null;
-};
+const backdrop = (props) => (
+  props.show ? <div className={styles.Backdrop} onClick={props.clicked}></div> : null
+);
 
 export default backdrop;
