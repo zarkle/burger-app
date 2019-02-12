93c81af096681e6bd181cc6746d257f66d5516fa
Beverly Pham <beverly.pham@gmail.com>
2019-02-12 15:28:03 -0800

    make drawer toggle component and add to layout

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,10 +2,11 @@ import React from 'react';
 import styles from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavItems from '../NavItems/NavItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => (
   <header className={styles.Toolbar}>
-    <div>MENU</div>
+    <DrawerToggle />
     <div className={styles.Logo}>
       <Logo />
     </div>
