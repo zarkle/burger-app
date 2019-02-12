93f279239b5e0703c530caf51a26715bd9786f56
Beverly Pham <beverly.pham@gmail.com>
2019-02-12 14:52:52 -0800

    clean up code

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,7 +6,7 @@ import NavItems from '../../Navigation/NavItems/NavItems';
 const sideDrawer = (props) => {
   
   return (
-    <div className={styles.sideDrawer}>
+    <div className={styles.SideDrawer}>
       <div className={styles.Logo}>
         <Logo />
       </div>
