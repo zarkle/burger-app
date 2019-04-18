eff62f866447ee76e66727b3e7e2687552568665
Beverly Pham <beverly.pham@gmail.com>
2019-04-17 18:05:06 -0700

    Updates

---
+++ precache-manifest.e82520dc4a8f2f86b4df84dbc3f48bd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5aae85e66f545ed55eb",
+    "url": "/burger-app/static/css/main.be5d44b3.chunk.css"
+  },
+  {
+    "revision": "c5aae85e66f545ed55eb",
+    "url": "/burger-app/static/js/main.c5aae85e.chunk.js"
+  },
+  {
+    "revision": "54252f1a25fc928b3cc2",
+    "url": "/burger-app/static/js/1.54252f1a.chunk.js"
+  },
+  {
+    "revision": "970a8769db42c56151f2",
+    "url": "/burger-app/static/js/runtime~main.970a8769.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-app/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "ceb9792fb636cec9aea2199e8b1f6833",
+    "url": "/burger-app/index.html"
+  }
+];
